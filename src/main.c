6fb7e6bf864860548c94e500fd0ee332eb3bf8a2
Thomas Cort <tcort@tcort.dev>
2022-06-01 11:16:21 +0000

    build: basic autotools build system
    Ref US0000A

---
+++ src/main.c
@@ -0,0 +1,36 @@
+/*
+
+  tcbasic - an interpreter for the programming language BASIC
+  Copyright (C) 2022 Thomas Cort
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#include "config.h"
+
+#include <stdlib.h>
+
+/*
+ * Main Program Entry Point
+ *
+ * Parameters:
+ *  - argc denotes the length of argv[]
+ *  - argv[] contains a list of command line fragments
+ *
+ * Return: exit code
+ */
+int main(int argc, char *argv[]) {
+	return EXIT_SUCCESS;
+}
